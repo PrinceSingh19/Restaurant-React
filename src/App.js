67091dad4decd480871691a80f5c2e985d73c6da
Prince Singh <princesingh808087@gmail.com>
2022-10-11 00:22:25 +0530

    Added initial bootstrap and created navbar brand with the help of reactstrap and bootstrap

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import {Navbar, NavbarBrand} from 'reactstrap';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React with Coursera
-        </a>
-      </header>
-    </div>
+      <div>
+        <Navbar dark color='primary' className='my-2'>
+       
+          <NavbarBrand href='./' className='mx-2'>
+            Ristorante De Confusion
+          </NavbarBrand>
+  
+      </Navbar>
+      </div>
   );
 }
 
