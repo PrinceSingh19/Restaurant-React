61caa2b976d4588ee3142843671415d39b7d5f8a
Prince Singh <princesingh808087@gmail.com>
2022-10-14 17:39:57 +0530

    React Components Part 1

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-import React from 'react';
-import './App.css';
-import {Navbar, NavbarBrand} from 'reactstrap';
+import React from "react";
+import "./App.css";
+import { Navbar, NavbarBrand } from "reactstrap";
+import Menu from "./components/Menu";
 
 function App() {
-  return (
-      <div>
-        <Navbar dark color='primary' className='my-2'>
-       
-          <NavbarBrand href='./' className='mx-2'>
-            Ristorante De Confusion
-          </NavbarBrand>
-  
-      </Navbar>
-      </div>
-  );
+	return (
+		<>
+			<Navbar dark color="primary">
+				<NavbarBrand href="./" className="mx-5">
+					Ristorante De Confusion
+				</NavbarBrand>
+			</Navbar>
+			<Menu />
+		</>
+	);
 }
 
 export default App;
