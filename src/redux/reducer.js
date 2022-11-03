b9652e3e7f19d20b5b8d34c40ccf93ed37810a18
Prince Singh <princesingh808087@gmail.com>
2022-11-03 21:06:21 +0530

    started using redux in the app

---
+++ src/redux/reducer.js
@@ -0,0 +1,6 @@
+import { DISHES } from "../shared/dishes";
+import { COMMENTS } from "../shared/comments";
+import { PROMOTIONS } from "../shared/promotions";
+import { LEADERS } from "../shared/leaders";
+
+export const initialState = {};
