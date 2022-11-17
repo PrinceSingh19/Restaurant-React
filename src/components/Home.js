1d68440bbee41adb4f5cbae6769da30f19e13b96
Prince Singh <princesingh808087@gmail.com>
2022-11-17 22:43:30 +0530

    Added framer motion

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,6 +5,7 @@ import { baseUrl } from "../shared/baseUrl";
 import { Loading } from "./Loading";
 
 import { Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle } from "reactstrap";
+import { motion } from "framer-motion";
 
 function RenderCard({ item, isLoading, isError }) {
 	if (isLoading) {
@@ -25,14 +26,30 @@ function RenderCard({ item, isLoading, isError }) {
 	}
 
 	return (
-		<Card>
-			<CardImg src={baseUrl + item.image} alt={item.name} />
-			<CardBody>
-				<CardTitle>{item.name}</CardTitle>
-				{item.designation ? <CardSubtitle>{item.designation}</CardSubtitle> : null}
-				<CardText>{item.description}</CardText>
-			</CardBody>
-		</Card>
+		<motion.div
+			initial="hidden"
+			whileInView="visible"
+			viewport={{ once: true }}
+			//transition={{ duration: 1 }}
+			//animate={{ opacity: 0 }}
+			transition={{ fadeIn: [0.17, 0.67, 0.83, 0.67], duration: 1 }}
+			variants={{
+				visible: {
+					opacity: [0.17, 0.67, 0.83, 1],
+					scale: 1,
+				},
+				hidden: { opacity: 0, scale: 0 },
+			}}
+		>
+			<Card>
+				<CardImg src={baseUrl + item.image} alt={item.name} />
+				<CardBody>
+					<CardTitle>{item.name}</CardTitle>
+					{item.designation ? <CardSubtitle>{item.designation}</CardSubtitle> : null}
+					<CardText>{item.description}</CardText>
+				</CardBody>
+			</Card>
+		</motion.div>
 	);
 }
 
@@ -40,6 +57,23 @@ function Home(props) {
 	const { isLoading, errMsg } = useSelector((state) => state.dishes);
 	const { promoLoading, errPromo } = useSelector((state) => state.promotions);
 	const { leadersLoading, errLeaders } = useSelector((state) => state.leaders);
+
+	/* if (isLoading || promoLoading || leadersLoading) {
+		return (
+			<>
+				<div className="container">
+					<div className="row">
+						<div>
+							<Loading />
+						</div>
+					</div>
+				</div>
+			</>
+		);
+	}
+	if (errMsg || errLeaders || errPromo) {
+		return <h5>{errMsg}</h5>;
+	} */
 
 	return (
 		<div className="container">
